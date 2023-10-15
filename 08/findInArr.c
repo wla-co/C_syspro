60e1a1b68f681bae5aab0d50616768b383b5c551
Vladislav Koskov <vladkoskov2005@gmail.com>
2023-10-15 19:42:42 +0700

    Place 08_1 and 08_2 here

--- 08/findInArr.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int* findInArr(int* arr, size_t len, int x) {
-    for (size_t i = 0; i < len; i++) {
-        if (arr[i] == x)
-            return &arr[i];
-    }
-    return NULL;
-}
--- 08/printArr.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-void printArr(int* arr, size_t len) {
-    for (size_t i = 0; i < len; i++) {
-        printf("%d\n", arr[i]);
-    }
-}
